61be068dcce3ce36127d5e69da57f9940fb1ab7e
Tatiana <tsg1204@gmail.com>
2017-10-09 15:46:17 -0400

    init commit

---
+++ app/index.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>Hello React Training!</div>
+		)
+	}
+
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
